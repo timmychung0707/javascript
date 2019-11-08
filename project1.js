04156b15ebbeb086d9b2e40b5f36341daa290212
mrkung <jkung@pacificamerican.org>
2019-11-08 08:53:04 +0800

    updated with project 1 guidelines

---
+++ project1.js
@@ -0,0 +1,12 @@
+// Project 1
+// Write a story that uses variables
+// 1. Use document.write(); to write your story onto the HTML page
+// 2. Make your story exciting
+// 3. Join the variables into your story
+// 4. Edit the variables so that your story is changeable
+// 5. Use 5 or more variables in your story
+
+// Story Prompt:
+// You're scrolling through the pictures on your phone. 
+// You notice a new one: it's of you sleeping.
+
