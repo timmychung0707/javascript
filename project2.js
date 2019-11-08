e7d03f34ce378473491973632877b38e4227206f
mrkung <jkung@pacificamerican.org>
2019-11-08 15:53:10 +0800

    renamed javascript.css to style.css, removed hello.js, updated project 1 with sample, began working on project 2

---
+++ project2.js
@@ -0,0 +1,14 @@
+// Project 2
+// Create a simple calculator that can add, subtract, multiply, and divide two numbers
+
+// HTML & CSS
+// 1. Create a new HTML document
+// 2. Create a div for your calculator "screen"
+// 3. Make sure each div has a different class so we can select it
+// 4. Create 4 divs for 4 buttons (add, subtract, multiply, divide)
+// 5. Create an HTML element that can display the two numbers we will work with
+// 6. Use CSS to make everything look pretty.
+
+// JAVASCRIPT
+// 1. You will need to create two variables to store each number
+// 2. You wil need to 
