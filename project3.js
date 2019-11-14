0d9146b803813ec8eff5e30dd7225cdde1ac840b
mrkung <jkung@pacificamerican.org>
2019-11-14 16:23:41 +0800

    added project 3

---
+++ project3.js
@@ -0,0 +1,8 @@
+// Create an online calculator that can add, subtract, multiply, and divide
+// You will need to use your HTML and CSS skills to build a nice looking calculator
+// After, you will add javascript to make it functional
+// 1. Your calculator must display the numbers being operated on
+// 2. Create a button for each operation
+// 3. Create a display to show the answer
+// 4. When dividing, you must display the answer as the quotient and remainder
+
