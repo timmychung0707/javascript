6a2540212a5af70efd14d7f21a46dd00821028b8
mrkung <jkung@pacificamerican.org>
2019-11-15 11:03:48 +0800

    updated project 3 and 4

--- project1.js
+++ project1.js
@@ -2,8 +2,8 @@
 // Write a story that uses variables
 // 1. Use document.write(); to write your story onto the HTML page
 // 2. Make your story exciting
-// 3. Join the variables into your story
-// 4. Edit the variables so that your story is changeable
+// 3. Join the variables into your story using concatenation
+// 4. Use prompts to retrieve a value for your variables
 // 5. Use 5 or more variables in your story
 
 // Story Prompt:
--- project3.js
+++ project3.js
@@ -1,8 +1,11 @@
-// Create an online calculator that can add, subtract, multiply, and divide
-// You will need to use your HTML and CSS skills to build a nice looking calculator
-// After, you will add javascript to make it functional
-// 1. Your calculator must display the numbers being operated on
-// 2. Create a button for each operation
-// 3. Create a display to show the answer
-// 4. When dividing, you must display the answer as the quotient and remainder
+/*
+You are trying to compare your BMI (Body Mass Index) with your friend's BMI.
+BMI is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meters).
 
+1. Store you and your friend's mass and height in variables
+2. Calculate both BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+GOOD LUCK ‍💪
+*/
