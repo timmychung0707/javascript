2db3da9f1e89aaa4e80cec735d1a0364aedbe841
mrkung <jkung@pacificamerican.org>
2019-11-15 11:19:00 +0800

    updated project 3 guidelines

--- project3.js
+++ project3.js
@@ -1,11 +1,13 @@
-/*
+/* HEALTHY BMI is between 18.5 to 24.9!
 You are trying to compare your BMI (Body Mass Index) with your friend's BMI.
 BMI is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meters).
 
 1. Store you and your friend's mass and height in variables
 2. Calculate both BMIs
-3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+3. Create an if else statement containing information about whether you have a higher BMI than your friend.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+BONUS: tell if you and your friend have a healthy BMI
 
 GOOD LUCK ‍💪
 */
