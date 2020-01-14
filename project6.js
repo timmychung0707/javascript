2e656e76b9c03a382f5d024df6d86207cd189536
mrkung <jkung@pacificamerican.org>
2020-01-14 10:51:30 +0800

    added project6

---
+++ project6.js
@@ -0,0 +1,15 @@
+/*
+You will be creating a secret passcode for Dino's Lair.
+
+1. Your passcode must take 4 numbers as inputs
+2. Store the passcode in a variable
+3. Check if the passcode is correct, then display a message
+
+CHALLENGE: 
+- Create a password that takes one word as input
+- The user must have the password AND passcode correct to enter Dino's Lair.
+
+*** Make sure to add some CSS to your passcode page to make it look nice and neat. ***
+
+GOOD LUCK 🤹‍♂️
+*/
