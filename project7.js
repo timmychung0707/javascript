c744db18e1e1337fe2bc967eedbacd46c2cd8cce
mrkung <jkung@pacificamerican.org>
2020-01-17 10:39:26 +0800

    added project 7, updated html file

---
+++ project7.js
@@ -0,0 +1,32 @@
+/*
+Welcome to Dino's Looping Challenge of Doom!
+You will need to create 4 loops for this challenge
+
+1. Create a loop that writes "I love homework" 5 times
+2. Create a loop that counts from 0 to 4 and writes the current number on the screen.
+3. Create a loop that counts from 15 to 30 but only writes multiples of 3 on the screen
+4. Create a loop that will draw 4X4 square using ⬜. It should look like this:
+⬜⬜⬜⬜
+⬜⬜⬜⬜
+⬜⬜⬜⬜
+⬜⬜⬜⬜
+(HINT: Loops can be nested inside another loop)
+
+
+CHALLENGE:
+4. Dino is a very neat and organized dinosaur. He likes to stack the bones (🦴) of his victims in a nice pyramid shape. Create a stack that is 7 bones high.
+
+- You will need to create a loop writes 1 🦴, and every successive line adds one more bone. It should look like this: 
+
+      🦴
+     🦴🦴
+    🦴🦴🦴
+   🦴🦴🦴🦴
+  🦴🦴🦴🦴🦴 
+ 🦴🦴🦴🦴🦴🦴
+🦴🦴🦴🦴🦴🦴🦴
+
+HINT: Loops can be nested inside another loop
+
+GOOD LUCK 🤹‍♂️
+*/
