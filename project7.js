e7779aced0cfc447a68539b87d4d81a945aaaa05
mrkung <jkung@pacificamerican.org>
2020-01-21 11:53:47 +0800

    added loops notes

--- project7.js
+++ project7.js
@@ -5,11 +5,12 @@ You will need to create 4 loops for this challenge
 1. Create a loop that writes "I love homework" 5 times
 2. Create a loop that counts from 0 to 4 and writes the current number on the screen.
 3. Create a loop that counts from 15 to 30 but only writes multiples of 3 on the screen
-4. Create a loop that will draw 4X4 square using ⬜. It should look like this:
+4. Create a loop that will draw 4X4 square using ⬜. ONLY PRINT 1 ⬜ at a time. Your loops should do the rest. It should look like this:
 ⬜⬜⬜⬜
 ⬜⬜⬜⬜
 ⬜⬜⬜⬜
 ⬜⬜⬜⬜
+document.write("⬜⬜⬜⬜") (X) <-- don't do this)
 (HINT: Loops can be nested inside another loop)
 
 
